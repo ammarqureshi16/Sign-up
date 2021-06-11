990b7ffd3f0757e97b219b295b95d5a18f2000a3
Ammar Qureshi <qureshiaq2003@gmail.com>
2021-06-11 16:16:36 +0500

    good

--- test app fire.js
+++ test app fire.js
@@ -1,27 +1,28 @@
-// function z(){
-//     const naam=document.getElementById("naam-input").value
-//     const number=document.getElementById("number-input").value
-//     const email=document.getElementById("email-input").value
-//     const password=document.getElementById("password-input").value
-//     document.getElementById("naam-input").value=""
-//     document.getElementById("number-input").value=""
-//     document.getElementById("email-input").value=""
-//     document.getElementById("password-input").value=""
-//     console.log(" Name :",naam)
-//     console.log(" Email : ",email)
-//     console.log(naam," Number :",number)
-//     console.log(" Password : ",password)
+function signup(){
+  const email=document.getElementById("email-input").value
+  const password=document.getElementById("password-input").value
+  document.getElementById("email-input").value=""
+  document.getElementById("password-input").value=""
+  console.log(" Email : "+email)
+  console.log(" Password : "+password) 
 
-//     firebase.auth().createUserWithEmailAndPassword(email,password)
-//   .then((user) => {
-//       alert(" Signup Successfully ")
-//   })
-//   .catch((error) => {
-//     var errorCode = error.code;
-//     var errorMessage = error.message;
-//     alert(errorMessage)
-//   });
-// }
+  firebase.auth().signInWithEmailAndPassword(email,password)
+  .then((user) => {
+      alert(" Signup Successfully ")
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert(errorMessage)
+  });
+}
+
+function gotofacebook(){
+  location.href="https://www.facebook.com/"
+}
+function gototwitter(){
+  location.href="https://twitter.com/explore"
+}
 
 function login(){
   const email=document.getElementById("email-input").value
@@ -29,11 +30,11 @@ function login(){
   document.getElementById("email-input").value=""
   document.getElementById("password-input").value=""
   console.log(" Email : "+email)
-  console.log(" Password : "+password)
+  console.log(" Password : "+password) 
 
   firebase.auth().signInWithEmailAndPassword(email,password)
   .then((user) => {
-      alert(" Signup Successfully ")
+      alert(" Login Successfully ")
   })
   .catch((error) => {
     var errorCode = error.code;
